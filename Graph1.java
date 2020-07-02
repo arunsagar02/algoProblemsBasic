357d43a45ffc74fea28ac9eafe7e19846b9f3245
arun <arunsagarnune02@gmail.com>
2020-07-02 17:18:07 +0530

    Added 2 new methods in Graph1
    Added BFS and DFS to the Graph1.java file

--- Graph1.java
+++ Graph1.java
@@ -38,18 +38,81 @@ public class Graph1
 			System.out.println();
 		}
 	}
+	public void bfs(int n)
+	{
+		System.out.println("The Nodes reachable from Node "+ n +" are");
+		bfs(graph,n); 
+	}
+	private void bfs(ArrayList<ArrayList<Integer>> adj, int s)
+	{
+		Queue<Integer> que = new LinkedList<Integer>();
+		boolean[] visited = new boolean[adj.size()];
+		visited[s] = true;
+		que.add(s);
+		while(!que.isEmpty())
+		{
+			int temp = que.poll();
+			for(int i = 0 ; i < adj.get(temp).size() ; ++i)
+			{
+				if(!visited[adj.get(temp).get(i)])
+				{
+					visited[adj.get(temp).get(i)] = true;
+					que.add(adj.get(temp).get(i));
+				}
+			}
+		}
+		for(int i = 0 ; i < visited.length ; ++i)
+		{
+			if(visited[i]) System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+	public void dfs(int n)
+	{
+		System.out.println("The Nodes reachable from Node "+ n +" are");
+		dfs(graph,n);
+	}
+	private void dfs(ArrayList<ArrayList<Integer>> adj, int s)
+	{
+		boolean[] visited = new boolean[adj.size()];
+		Stack<Integer> stk = new Stack<Integer>();
+		visited[s] = true;
+		stk.push(s);
+		while(!stk.isEmpty())
+		{
+			int temp = stk.pop();
+			for(int i = 0 ; i < adj.get(temp).size() ; ++i)
+			{
+				if(!visited[adj.get(temp).get(i)])
+				{
+					visited[adj.get(temp).get(i)] = true;
+					stk.push(adj.get(temp).get(i));
+				}
+			}
+		}
+		for(int i = 0 ; i < visited.length ; ++i)
+		{
+			if(visited[i]) System.out.print(i+" ");
+		}
+		System.out.println();
+	}
 	
 }
 class Main
 {
 	public static void main(String[] args)
 	{
-		Graph1 g = new Graph1(5);
+		Graph1 g = new Graph1(7);
 		g.addEdge(0,1);
 		g.addEdge(2,3);
 		g.addEdge(3,4);
 		g.addEdge(4,0);
 		g.addEdge(4,1);
+		g.addEdge(6,5);
 		g.printGraph();
+		g.bfs(2);
+		g.bfs(6);
+		g.dfs(2);
+		g.dfs(5);
 	}
 }
